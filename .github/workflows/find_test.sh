e0b40fe5ec6cac95f2995cc4168f7a10773de214
Chris Schnaufer <schnaufer@email.arizona.edu>
2020-08-19 11:27:15 -0700

    9

--- .github/workflows/find_test.sh
+++ .github/workflows/find_test.sh
@@ -25,7 +25,8 @@ if [[ "${#FOLDER_LIST[@]}" == "${EXPECTED_NUM_FOLDERS}" ]]; then
   echo "Found expected number of folders: ${EXPECTED_NUM_FOLDERS}"
 else
   echo "Expected ${EXPECTED_NUM_FOLDERS} folders and found ${#FOLDER_LIST[@]}"
-  for i in $(seq 0 (( ${#FOLDER_LIST[@]} - 1)))
+  COUNT=$(( ${#FOLDER_LIST[@]} - 1 ))
+  for i in $(seq 0 $COUNT)
   do
     echo "${i}: ${FOLDER_LIST[$i]}"
   done
