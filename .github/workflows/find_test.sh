be02a86907a2caeb5ba26b3530c29cc247efe85b
Chris Schnaufer <schnaufer@email.arizona.edu>
2020-08-19 11:32:35 -0700

    12

--- .github/workflows/find_test.sh
+++ .github/workflows/find_test.sh
@@ -3,7 +3,7 @@
 TARGET_FOLDER="outputs"
 # Define some counts that we expect (the number of sub-folders plus the top folder)
 EXPECTED_NUM_FOLDERS=65
-EXPECTED_NUM_CANOPYCOVER_CSV=56
+EXPECTED_NUM_CANOPYCOVER_CSV=64
 # The same number of CSV files + original, un-clipped mask file
 EXPECTED_NUM_MASK_TIF=$((EXPECTED_NUM_CANOPYCOVER_CSV + 1))
 
@@ -27,7 +27,7 @@ else
   echo "Expected ${EXPECTED_NUM_FOLDERS} folders and found ${#FOLDER_LIST[@]}"
   for i in $(seq 0 $(( ${#FOLDER_LIST[@]} - 1 )))
   do
-    echo "${i}: ${FOLDER_LIST[$i]}"
+    echo "$(( ${i} + 1 )): ${FOLDER_LIST[$i]}"
   done
   exit 10
 fi
@@ -40,7 +40,7 @@ else
   echo "Expected ${EXPECTED_NUM_CANOPYCOVER_CSV} canopycover.csv files but found ${#EXPECTED_CSV[@]}"
   for i in $(seq 0 $(( ${#EXPECTED_CSV[@]} - 1 )))
   do
-    echo "${i}: ${EXPECTED_CSV[$i]}"
+    echo "$(( ${i} + 1 )): ${EXPECTED_CSV[$i]}"
   done
   exit 20
 fi
@@ -53,7 +53,7 @@ else
   echo "Expected ${EXPECTED_NUM_MASK_TIF} orthomosaic_mask.tif files but found ${#EXPECTED_MASK[@]}"
   for i in $(seq 0 $(( ${#EXPECTED_MASK[@]} - 1 )))
   do
-    echo "${i}: ${EXPECTED_MASK[$i]}"
+    echo "$(( ${i} + 1 )): ${EXPECTED_MASK[$i]}"
   done
   exit 30
 fi
