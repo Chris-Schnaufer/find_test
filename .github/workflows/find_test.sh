e913baa8bac72a13b9399ff9a36d1b4d25d72830
Chris Schnaufer <schnaufer@email.arizona.edu>
2020-08-19 11:11:45 -0700

    Checking file/folder finding

--- .github/workflows/find_test.sh
+++ .github/workflows/find_test.sh
@@ -1,3 +1,51 @@
 #!/bin/bash
 
-find "./" -maxdepth 1 -type d
+TARGET_FOLDER="outputs"
+# Define some counts that we expect
+EXPECTED_NUM_FOLDERS=64
+EXPECTED_NUM_CANOPYCOVER_CSV=56
+# The same number of CSV files + original, un-clipped mask file
+EXPECTED_NUM_MASK_TIF=$((EXPECTED_NUM_CANOPYCOVER_CSV + 1))
+
+mkdir "${TARGET_FOLDER}"
+for i in $(seq 1 64)
+do
+  mkdir "${TARGET_FOLDER}/${i}"
+  touch "${TARGET_FOLDER}/${i}/canopycover.csv"
+  touch "${TARGET_FOLDER}/${i}/orthomosaic_mask.tif"
+done
+
+ls -ld "${TARGET_FOLDER}"
+ls -ld "${TARGET_FOLDER}/1"
+
+
+# Get all the folders and check the count
+FOLDER_LIST=(`find "${TARGET_FOLDER}" -maxdepth 1 -type d`)
+if [[ "${#FOLDER_LIST[@]}" == "${EXPECTED_NUM_FOLDERS}" ]]; then
+  echo "Found expected number of folders: ${EXPECTED_NUM_FOLDERS}"
+else
+  echo "Expected ${EXPECTED_NUM_FOLDERS} folders and found ${#FOLDER_LIST[@]}"
+  echo ${FOLDER_LIST}
+  exit 10
+fi
+
+# Check the expected number of output files
+EXPECTED_CSV=(`find "${TARGET_FOLDER}" -type f | grep 'canopycover\.csv'`)
+if [[ "${#EXPECTED_CSV[@]}" == "${EXPECTED_NUM_CANOPYCOVER_CSV}" ]]; then
+  echo "Found expected number of canopycover.csv files: ${EXPECTED_NUM_CANOPYCOVER_CSV}"
+else
+  echo "Expected ${EXPECTED_NUM_CANOPYCOVER_CSV} canopycover.csv files but found ${#EXPECTED_CSV[@]}"
+  echo ${EXPECTED_CSV}
+  exit 20
+fi
+
+# Check the expected number of image mask files
+EXPECTED_MASK=(`find "${TARGET_FOLDER}" -type f | grep 'orthomosaic_mask\.tif'`)
+if [[ "${#EXPECTED_MASK[@]}" == "${EXPECTED_NUM_MASK_TIF}" ]]; then
+  echo "Found expected number of orthomosaic_mask.tif files: ${EXPECTED_NUM_MASK_TIF}"
+else
+  echo "Expected ${EXPECTED_NUM_MASK_TIF} orthomosaic_mask.tif files but found ${#EXPECTED_MASK[@]}"
+  echo ${EXPECTED_MASK}
+  exit 30
+fi
+
